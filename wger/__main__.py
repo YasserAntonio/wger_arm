4f1883409bfcc531fdad7498c5a76702ff6d4fd2
YasserAntonio <alfredo.ai.app@gmail.com>
2020-12-24 17:31:52 +0100

    update path on main

--- wger/__main__.py
+++ wger/__main__.py
@@ -33,7 +33,7 @@ invoke_cmd = 'invoke '
 
 def main():
     # Change the working directory so that invoke can find the tasks file
-    os.chdir('/home/wger/wger')
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     args = sys.argv[1:]
     if len(args):
